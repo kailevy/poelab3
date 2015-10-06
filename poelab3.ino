4ed4badad4be6d6f10d9b249add6671324a458de
kailevy <kai0levy@gmail.com>
2015-10-06 19:07:26 -0400

    Make preliminary proportional control

--- poelab3.ino
+++ poelab3.ino
@@ -7,30 +7,31 @@ Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
 byte rightSensorPin = A0;
 byte leftSensorPin = A1;
-int senseThreshold = 1000; //threshold for if it's over tape
+int senseThreshold = 30; //threshold for if one is too much
 
 
 void setup() {
   Serial.begin(9600);
   AFMS.begin();
-  rightMotor->setSpeed(100);
-  leftMotor->setSpeed(100);
 }
 
 
 void loop() {
   int rightSense = analogRead(rightSensorPin);
   int leftSense = analogRead(leftSensorPin);
+  int diffSense = rightSense-leftSense;
+  int diffSpeed = 0;
   
+  if (diffSense > senseThreshold){
+    diffSpeed = diffSense/2;
+  }
+  else if (diffSense < -senseThreshold){
+    diffSpeed = -diffSense/2;
+  }
   
-  if (leftSense > senseThreshold) {
-    leftMotor->run(RELEASE);
-  }
-  else if (rightSense > senseThreshold) {
-    rightMotor->run(RELEASE);
-  }  
-  else {
-    leftMotor->run(FORWARD);
-    rightMotor->run(FORWARD);
-  }
+  rightMotor->setSpeed(100 + diffSpeed);
+  leftMotor->setSpeed(100 - diffSpeed);
+  
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
 }
