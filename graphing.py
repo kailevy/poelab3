7e27d9cbe64c4bc351609d86b3af13a6be1452a3
Keenan Zucker <Keenan.Zucker@students.olin.edu>
2015-10-11 18:56:15 -0400

    minor changes to graphing

--- graphing.py
+++ graphing.py
@@ -2,22 +2,30 @@ import matplotlib.pyplot as plt
 import time
 import serial
 
-port = '/dev/ttyUSB0'									# Arduino Port Number
-frequency = '9600'										# Frequency
 
-ser = serial.Serial(port,frequency,timeout=2)  
-time.sleep(2)
 
-f = open('data.txt','w')
+def gatherData():
+	"""Collects data from serial output of Arduino and writes it to a text file called
+	'data.txt'. Runs until user stops output."""
 
-while True:
-	if ser.inWaiting():
-		try:
-			rawData = ser.readline()
-			f.write(rawData)
-			time.sleep(0.2)
-		except:
-			time.sleep(0.2)
+	port = '/dev/ttyUSB0'									# Arduino Port Number
+	frequency = '9600'										# Frequency
 
-#f.write(rawData)
-f.close()
+	ser = serial.Serial(port,frequency,timeout=2)  
+	time.sleep(2)
+
+	f = open('data.txt','w')
+
+	while True:
+		if ser.inWaiting():
+			try:
+				rawData = ser.readline()
+				f.write(rawData)
+				time.sleep(0.2)
+			except:
+				time.sleep(0.2)
+
+	#f.write(rawData)
+	f.close()
+
+
